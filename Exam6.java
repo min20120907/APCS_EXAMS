73cd4b05817c49b3da168268a162cd7e3d3a94f4
min20120907 <jefflin.je598@gmail.com>
2017-11-15 15:59:45 +0800

    Create Exam6.java

---
+++ Exam6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author min20120907
+ */
+public class Exam6 {
+
+    /**
+     * @param args the command line arguments
+     */
+ public static void main(String[] args) {
+        Scanner s2 = new Scanner(System.in);
+        System.out.println("請輸入NMK:");
+        student=s2.nextString();
+        String input = s.nextLine();
+        String[] group = input.split("\\s+");
+        int[] NumGroup =StringArrToIntArr(group);
+        
+    
+}
